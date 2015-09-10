2c00e9518c3446f66b3421a2b2dbe170af488d81
Jacob Heil <jheil@wooster.edu>
2015-09-10 11:08:56 -0400

    replaced my bootstrap stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Hello!</h1>
+
+I'm using this space to learn bootstrap and git, so it'll grow into a site over time. For now it's just kind of here. 
--- js/ui.js
+++
@@ -1,9 +0,0 @@
-  $( "#toggleTimeline" ).click(function () {
-  if ( $( "#visualization" ).is( ":hidden" ) ) {
-    $( "#visualization" ).slideDown( "fast" );
-    $("#toggleTimeline i").attr("class","fa fa-eye-slash")
-  } else {
-    $( "#visualization" ).hide();
-    $("#toggleTimeline i").attr("class","fa fa-eye")
-  }
-});
